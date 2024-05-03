124d64471329df5fd9153ba6ded050dec575a9cd
Abass Koyang <abasskoyang12345@gmail.com>
2024-05-03 17:51:49 +0100

    add: heading and description to homee page, cta to home page. Create all components needed in Customization page - AiPicker, filePicker, ColorPicker, CustomButton, Tab

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home"
 function App() {
 
   return (
-     <main>
+     <main className='app transition-all ease-in'>
       <Home />
       <Canvas />
       <Customizer/>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import {useSnapshot} from 'valtio';
 import {slideAnimation, headContainerAnimation, headContentAnimation, headTextAnimation} from '../config/motion';
 
 import state from '../store/index';
+import { CustomButton } from '../components';
 
 const Home = () => {
     const snap = useSnapshot(state);
@@ -19,8 +20,19 @@ const Home = () => {
                 </motion.header>
                 <motion.div className='home-content' {...headContainerAnimation}>
                     <motion.div {...headTextAnimation}>
-                        <h1 className='head-text'>LET'S <br className='hidden xl:block'/>  CREATE SOMETHING TOGETHER
+                        <h1 className='head-text'>LET'S <br className='hidden xl:block'/> DO IT.
                         </h1>
+                    </motion.div>
+                    <motion.div className='flex flex-col gap-5' {...headContentAnimation}>
+                        <p className='max-w-md font-normal text-gray-600 text-base'>
+                            Create your unique and exclusive sgirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+                        </p>
+                        <CustomButton
+                            type="filled"
+                            title="Customize It"
+                            handleClick={()=> state.intro = false}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                         />
                     </motion.div>
                 </motion.div>
             </motion.section>
