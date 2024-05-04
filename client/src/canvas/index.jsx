ff7bf4f82a8c39eccd24de2d7e5116b883bdd44d
Abass Koyang <abasskoyang12345@gmail.com>
2024-05-04 08:28:50 +0100

    create shirt and dislay shirtt model

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,25 @@
+import {Canvas} from "@react-three/fiber";
+import {Environment, Center} from "@react-three/drei";
+
+import Backdrop from "./Backdrop";
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
 
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>index</div>
+    <Canvas>
+        <ambientLight intensity={0.5}/>
+        <Environment preset="city" />
+
+        <CameraRig>
+            {/* <Backdrop /> */}
+            <Center>
+                <Shirt />
+            </Center>
+        </CameraRig>
+    </Canvas>
   )
 }
 
-export default Canvas;
+export default CanvasModel;
